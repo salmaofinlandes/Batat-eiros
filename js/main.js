3e3856376d7d7b7927a08d0736ad4a7ba75ba4ae
SalmaoFinlandes <48417806+salmaofinlandes@users.noreply.github.com>
2021-05-06 17:16:59 +0100

    opacity and grayscale added

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+function imageEffects(opacity, grayscale){
+  var i;
+  for(i=0; i< document.getElementsByTagName("img").length; i++){
+    if (document.getElementsByTagName("img")[i].style.display == "block"){
+      document.getElementsByTagName("img")[i].style.opacity = opacity;
+      document.getElementsByTagName("img")[i].style.filter = "grayscale("+ grayscale.toString() +"%)";
+    }
+    else{continue;}
+  }
+}
+
+function mouseOver(){
+  document.getElementById("button").style.opacity = 1;
+  imageEffects(0.7,70);
+}
+
+function mouseOut(){
+  document.getElementById("button").style.opacity = 0.5;
+  imageEffects(1,0);
+}
